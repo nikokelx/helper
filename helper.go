9b455fc96df3f8b8a92ec00697d88bd896b865a6
nikokelx <nikolaj.keller@protonmail.com>
2024-08-09 21:04:35 +0200

    Push helper functions

--- helper.go
+++ helper.go
@@ -1,5 +1,44 @@
 package helper
 
-func Hello(name string) string {
-    return "HEALS"
+import (
+    "encoding/json"
+    "net/http"
+    "database/sql"
+)
+
+func PanicIfError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+type WebResponse struct {
+	Code   int         `json:"code"`
+	Status string      `json:"status"`
+	Data   interface{} `json:"data,omitempty"`
+}
+
+func ReadRequestBody(r *http.Request, result interface{}) {
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(result)
+	PanicIfError(err)
+}
+
+func WriteResponseBody(write http.ResponseWriter, response interface{}) {
+	write.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(write)
+	err := encoder.Encode(response)
+	PanicIfError(err)
+}
+
+func CommitOrRollback(tx *sql.Tx) {
+    err := recover()
+
+    if err != nil {
+        errRollback := tx.Rollback()
+        PanicIfError(errRollback)
+        panic(err)
+    } else {
+        errCommit := tx.Commit()
+        PanicIfError(errCommit)
+    }
 }
