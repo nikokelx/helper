079534a618c3cb4ba0d359d7fae1cf788228b919
nikokelx <nikolaj.keller@protonmail.com>
2024-08-18 20:02:43 +0200

    Add verify session

--- helper.go
+++ helper.go
@@ -1,25 +1,40 @@
 package helper
 
 import (
-    "encoding/json"
-    "net/http"
-    "database/sql"
+	"database/sql"
+	"encoding/json"
+	"net/http"
 )
 
-func PanicIfError(err error) {
-    if err != nil {
-        panic(err)
-    }
+type SessionResponse struct {
+	Code   int    `json:"code"`
+	Status string `json:"status"`
+	Data   struct {
+		Token string `json:"token"`
+	} `json:"data"`
 }
+
 type WebResponse struct {
 	Code   int         `json:"code"`
 	Status string      `json:"status"`
 	Data   interface{} `json:"data,omitempty"`
 }
 
+func PanicIfError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func ReadRequestBody(r *http.Request, result interface{}) {
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(result)
+	PanicIfError(err)
+}
+
+func ReadResponseBody(r *http.Response, result interface{}) {
+	decode := json.NewDecoder(r.Body)
+	err := decode.Decode(result)
 	PanicIfError(err)
 }
 
@@ -31,14 +46,25 @@ func WriteResponseBody(write http.ResponseWriter, response interface{}) {
 }
 
 func CommitOrRollback(tx *sql.Tx) {
-    err := recover()
+	err := recover()
 
-    if err != nil {
-        errRollback := tx.Rollback()
-        PanicIfError(errRollback)
-        panic(err)
-    } else {
-        errCommit := tx.Commit()
-        PanicIfError(errCommit)
-    }
+	if err != nil {
+		errRollback := tx.Rollback()
+		PanicIfError(errRollback)
+		panic(err)
+	} else {
+		errCommit := tx.Commit()
+		PanicIfError(errCommit)
+	}
+}
+
+func VerifySession(url string) string {
+	response, err := http.Get(url)
+	PanicIfError(err)
+
+	session := new(SessionResponse)
+
+	ReadResponseBody(response, &session)
+
+	return session.Data.Token
 }
